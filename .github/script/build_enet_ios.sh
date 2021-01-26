534a0f3526663dd5018dcdec425b083d22107fd5
eagleychen(陈鹰) <tonychanchen@gmail.com>
2021-01-26 15:06:24 +0800

    触发ios pod发布

--- .github/script/build_enet_ios.sh
+++ .github/script/build_enet_ios.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rc=$(git rev-parse --short HEAD)
+echo $rc
+
 gpg --quiet -d --passphrase "$PROVISIONING_PASSWORD" --batch .github/file/CMakeLists.txt.asc > .github/file/CMakeLists.txt
 
 git branch
@@ -13,26 +16,24 @@ cd iot-p2p
 
 git checkout $GIT_BRANCH_IMAGE_VERSION
 
-rc=$(git rev-parse --short HEAD)
-echo $rc
 
-git config --global user.name "codefarmer"
-git config --global user.name "dev_tester@163.com"
+#git config --global user.name "codefarmer"
+#git config --global user.name "dev_tester@163.com"
 
-git config --list
+#git config --list
 
-datetime=$(date '+%Y-%m-%d %H:%M:%S')
-echo $datetime >> readme.md
+#datetime=$(date '+%Y-%m-%d %H:%M:%S')
+#echo $datetime >> readme.md
+#
+#echo $datetime
 
-echo $datetime
+#git add .
+#git commit -m "tencentyun/iot-p2p@$rc"
+#echo ":wq" | git commit --amend --author='codefarmer <dev_tester@163.com>'
 
-git add .
-git commit -m "tencentyun/iot-p2p@$rc"
-echo ":wq" | git commit --amend --author='codefarmer <dev_tester@163.com>'
-
-echo "whhhhhhstart"
-git log -2
-echo "whhhhhhstend"
+#echo "whhhhhhstart"
+#git log -2
+#echo "whhhhhhstend"
 
 
 #if [ $1 == 'Release' ]; then
@@ -71,3 +72,24 @@ xcodebuild build -project eNet.xcodeproj -scheme enet_static -configuration Rele
 
 #strip -x -S Release-iphoneos/libenet.a -o  Release-iphoneos/libenet_.a
 #lipo -info Release-iphoneos/libenet.a
+
+
+
+
+
+
+#触发pod发布
+git clone https://$GIT_ACCESS_TOKEN@github.com/tonychanchen/TIoTThridSDK.git
+cd TIoTThridSDK
+
+cp ../Release-iphoneos/libenet.a  TIoTThridSDK/XP2P-iOS/libenet.a
+
+poddatetime=$(date '+%Y%m%d%H%M')
+echo $poddatetime
+
+git add .
+git commit -m "tencentyun/iot-p2p-build@$rc"
+git push https://$GIT_ACCESS_TOKEN@github.com/tonychanchen/TIoTThridSDK.git
+
+git tag "1.0.0-beta.$poddatetime"
+git push https://$GIT_ACCESS_TOKEN@github.com/tonychanchen/TIoTThridSDK.git --tags
