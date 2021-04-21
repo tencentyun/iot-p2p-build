4d532a4de969e0f2242567c1a23a8f887d46e9cd
archurtan <archurspace@gmail.com>
2021-04-21 09:37:01 +0800

    调整CI

--- .github/script/update_version_for_android.sh
+++ .github/script/update_version_for_android.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-version=$(tail -n 1 .github/file/android_release_version.txt)
-
-if [ "$version" = "snapshot" ]; then
-  echo "publish snapshot"
-else
-  echo "publish release"
-  sed -i 's#def libVersion.*#def libVersion = \"'$version'\"#g' iot-p2p/samples/android/xnet/build.gradle
-fi
+rtt=$(git describe --tags `git rev-list --tags --max-count=1`)
+rt=${rtt#*v}
+sed -i 's#def libVersion.*#def libVersion = \"'$rt'\"#g' iot-p2p/samples/android/xnet/build.gradle
