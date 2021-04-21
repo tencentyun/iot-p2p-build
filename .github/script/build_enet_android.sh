3e036801e14062528db49723f972622eb815fa7d
archurtan <archurspace@gmail.com>
2021-04-21 20:03:01 +0800

    update ci

--- .github/script/build_enet_android.sh
+++ .github/script/build_enet_android.sh
@@ -10,7 +10,11 @@ echo $GIT_BRANCH_IMAGE_VERSION
 # 1.拉取eNet支持库
 git clone https://$GIT_ACCESS_TOKEN@github.com/tencentyun/iot-p2p.git
 cd iot-p2p
-git checkout $rb
+if [ $1 == 'Release' ]; then
+	git checkout $GIT_BRANCH_IMAGE_VERSION
+else
+    git checkout $rb
+fi
 
 rc=$(git rev-parse --short HEAD)
 echo $rc
