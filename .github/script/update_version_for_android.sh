c2caf072d24cfd3bf9f6601ee67a4ae46f7b012a
archurtan <archurspace@gmail.com>
2021-04-22 19:22:43 +0800

    update ci

--- .github/script/update_version_for_android.sh
+++ .github/script/update_version_for_android.sh
@@ -1,5 +1,48 @@
 #!/bin/sh
 
-rtt=$(git describe --tags `git rev-list --tags --max-count=1`)
-rt=${rtt#*v}
-sed -i 's#def libVersion.*#def libVersion = \"'$rt'\"#g' iot-p2p/samples/android/xnet/build.gradle
+rtt=$GIT_BRANCH_IMAGE_VERSION
+rc=$(git rev-parse --short HEAD)
+rb=$(git rev-parse --abbrev-ref HEAD)
+currtag=$(git describe --tags `git rev-list --tags --max-count=1`)
+currbra=$rb
+echo 000---$currtag
+echo 111---$rtt
+echo 222---$rc
+echo 333---$rb
+
+# ==========此处添加版本自增逻辑，如果是持续集成发snapshot，最新tag+1；如果是发布就发branch
+vtag=${currtag#*v}
+echo $vtag
+
+
+branch=${currbra#*v}
+vbranch=${branch%x*}0
+echo $vbranch
+
+function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
+
+resultvv=$vbranch
+if version_ge $vtag $vbranch; then
+
+    echo "$vtag is greater than or equal to $vbranch"
+
+    vtaglist=(${vtag//./ })
+
+    firsttag=${vtaglist[0]}
+    secondtag=${vtaglist[1]}
+    thirdtag=${vtaglist[2]}
+    thirdtag=`expr $thirdtag + 1`
+
+    resultvv=$firsttag.$secondtag.$thirdtag
+fi
+
+echo "-->>$resultvv"
+
+if [ "$1" = "Debug" ]; then
+	sed -i 's#def libVersion.*#def libVersion = \"'$resultvv'-SNAPSHOT\"#g' iot-p2p/samples/android/xnet/build.gradle
+else
+	sed -i 's#def libVersion.*#def libVersion = \"'$vtag'\"#g' iot-p2p/samples/android/xnet/build.gradle
+fi
+# ==========此处添加版本自增逻辑，如果是持续集成发snapshot，最新tag+1；如果是发布就发branch
+
+
