57f4728bb6d39348a261e4135d8a2e7b1c384434
eagleychen(陈鹰) <tonychanchen@gmail.com>
2021-04-22 19:00:10 +0800

    snapshot版本号自增

--- .github/script/build_enet_ios.sh
+++ .github/script/build_enet_ios.sh
@@ -5,6 +5,9 @@ set -e
 rtt=$GIT_BRANCH_IMAGE_VERSION
 rc=$(git rev-parse --short HEAD)
 rb=$(git rev-parse --abbrev-ref HEAD)
+currtag=$(git describe --tags `git rev-list --tags --max-count=1`)
+currbra=$rb
+echo 000---$currtag
 echo 111---$rtt
 echo 222---$rc
 echo 333---$rb
@@ -94,5 +97,39 @@ git add .
 git commit -m "tencentyun/iot-p2p-build@$rc"
 git push https://$GIT_ACCESS_TOKEN@github.com/tonychanchen/TIoTThridSDK.git
 
-git tag "1.0.6-beta.$poddatetime"
+# ==========此处添加版本自增逻辑，如果是持续集成发snapshot，最新tag+1；如果是发布就发branch
+vtag=${currtag#*v}
+echo $vtag
+
+
+branch=${currbra#*v}
+vbranch=${branch%x*}0
+echo $vbranch
+
+function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
+
+resultvv=$vbranch
+if version_ge $vtag $vbranch; then
+    
+    echo "$vtag is greater than or equal to $vbranch"
+    
+    vtaglist=(${vtag//./ })
+    
+    firsttag=${vtaglist[0]}
+    secondtag=${vtaglist[1]}
+    thirdtag=${vtaglist[2]}
+    thirdtag=`expr $thirdtag + 1`
+    
+    resultvv=$firsttag.$secondtag.$thirdtag
+fi
+
+echo "-->>$resultvv"
+
+if [ $1 == 'Debug' ]; then
+    git tag "$resultvv-beta.$poddatetime"
+else
+    git tag "$resultvv"
+fi
+# ==========此处添加版本自增逻辑，如果是持续集成发snapshot，最新tag+1；如果是发布就发branch
+
 git push https://$GIT_ACCESS_TOKEN@github.com/tonychanchen/TIoTThridSDK.git --tags
