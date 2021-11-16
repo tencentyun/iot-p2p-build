91c9150227ec50a60d71413dc45c0aec768bda8e
tonychan <tonychanchen@gmail.com>
2021-11-16 14:28:48 +0800

    添加sign发布正式报

--- .github/script/update_values_for_sign.sh
+++ .github/script/update_values_for_sign.sh
@@ -6,3 +6,8 @@ root_path=$(pwd)
 sed -i 's#MY_KEY_ID#'$key_id'#g' iot-p2p/samples/android/gradle.properties
 sed -i 's#MY_PASSWORD#'$password'#g' iot-p2p/samples/android/gradle.properties
 sed -i 's#MY_KEY_RING_FILE#'$root_path'/secring.gpg#g' iot-p2p/samples/android/gradle.properties
+
+#device android
+sed -i 's#MY_KEY_ID#'$key_id'#g' iot-p2p/android_device/device_video_aar/gradle.properties
+sed -i 's#MY_PASSWORD#'$password'#g' iot-p2p/android_device/device_video_aar/gradle.properties
+sed -i 's#MY_KEY_RING_FILE#'$root_path'/secring.gpg#g' iot-p2p/android_device/device_video_aar/gradle.properties
