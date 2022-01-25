2f83162f29c108045f722244a38d15264bbbd179
tonychan <tonychanchen@gmail.com>
2022-01-25 15:42:02 +0800

    添加Linux sdk 编译环境

--- .github/script/build_enet_linux.sh
+++ .github/script/build_enet_linux.sh
@@ -1,25 +1,79 @@
 #!/bin/sh
+#set -eo pipefail
+set -e
 
-echo $GIT_BRANCH_IMAGE_VERSION
+rtt=$GIT_BRANCH_IMAGE_VERSION
+rc=$(git rev-parse --short HEAD)
+rb=$(git rev-parse --abbrev-ref HEAD)
+currtag=$(git describe --tags --match "v[0-9]*" --abbrev=0 HEAD)
+currbra=$rb
+echo 000---$currtag
+echo 111---$rtt
+echo 222---$rc
+echo 333---$rb
 
+
+#CURVERSION=$(git describe --tags `git rev-list --tags --max-count=1`) #获取tag
+#echo $CURVERSION
 # 1.拉取eNet支持库
 git clone https://$GIT_ACCESS_TOKEN@github.com/tencentyun/iot-p2p.git
 cd iot-p2p
-git checkout $GIT_BRANCH_IMAGE_VERSION
 
-rc=$(git rev-parse --short HEAD)
-echo $rc
+#2. 切换分支
+if [ $1 == 'Debug' ]; then
+    git checkout $rb --
+else
+    git checkout $rtt
+fi
 
-cd components_src/eNet
+#3. 获取pp版本号
+VIDEOSDKRC=$(git rev-parse --short HEAD)
+VIDEOSDKVERSION=$rb+git.$VIDEOSDKRC
+if [ $1 == 'Release' ]; then
+    VIDEOSDKVERSION=$rtt+git.$VIDEOSDKRC
+fi
+VIDEOSDKVERSION=${VIDEOSDKVERSION#*v}
+echo $VIDEOSDKVERSION
 
 
-# 2.编译Linux平台工程配置
-mkdir -p build/linux
+# 2.编译iOS平台工程配置
+mkdir -p build
 
-cd build/linux
+cd build
 
-cmake ../.. # 编译release版本
-#cmake ../.. -DCMAKE_BUILD_TYPE=DEBUG # 编译debug版本
-make  -j 8
+#cp ../../../.github/file/CMakeLists.txt   ../../CMakeLists.txt
+#perl -i -pe "s#.*armv7;armv7s;arm64.*#\t\tset(CMAKE_OSX_ARCHITECTURES \"arm64\" CACHE STRING \"\" FORCE)#g" ../../CMakeLists.txt
+#perl -i -pe "s#.*src/proc.*#\t\"src/proc/*\"\n\t\"src/app_interface/*\"#g" ../../CMakeLists.txt
+#perl -i -pe "s#.*bundle_static_library.*# #g" ../../CMakeLists.txt
 
-strip -x -S libenet.a -o  libenet_.a
+sed -i "" "s/.*VIDEOSDKVERSION.*/static const char * VIDEOSDKVERSION = \"$VIDEOSDKVERSION\";/g" ../samples/windows_p2p/app_interface/app_p2p/appWrapper.h
+
+#编译enet库
+cmake  -DCMAKE_BUILD_TYPE=Release -DENET_NO_STATIC_BINARY=ON -DBUNDLE_CERTS=OFF -DWITH_DHT=OFF -DBUILD_WITH_FS=OFF -DWITH_ZIP=OFF -DENET_VERSION=lts_1.3 ..
+cmake --build . --config Release
+
+cd ../samples/windows_p2p/
+
+#编译app_interface库
+mkdir build
+cd build
+
+cmake .. -DCOMPILE_SYSTEM=linux -DSYSTEM_ARCH=x86 -DCOMPILE_TYPE=Release
+cmake --build . --config Release
+ls app_interface/
+#libapp_interface.a
+
+mkdir xp2p_linux
+
+cp ../external_libs/lib/linux/x86/Release/libcurl.a          xp2p_linux/
+cp ../app_interface/app_p2p/appWrapper.h    xp2p_linux/AppWrapper.h
+cp app_interface/*.a                        xp2p_linux/
+cp libenet.a                                xp2p_linux/
+cp ../_deps/libevent-build/*.a              xp2p_linux/
+cp ../_deps/mbedtls-build/library/*.a       xp2p_linux/
+cp ../_deps/minizip-build/*.a               xp2p_linux/
+cp ../_deps/tinyxml2-build/*.a              xp2p_linux/
+cp ../_deps/zlib-build/*.a                  xp2p_linux/
+
+poddatetime=$(date '+%Y%m%d%H%M')
+echo $poddatetime
