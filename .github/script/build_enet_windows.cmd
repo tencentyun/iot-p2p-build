8fbde74bb1d196a16397bd7f49471d0b30ba00cf
archurtan <archurtan@tencent.com>
2022-02-17 18:14:32 +0800

    update windows workflows and batch script.

--- .github/script/build_enet_windows.cmd
+++ .github/script/build_enet_windows.cmd
@@ -1,4 +1,4 @@
-set PATH=C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x86\;%PATH%;
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
 
 set rtt=%2
 
