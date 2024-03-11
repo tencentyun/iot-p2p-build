8648b92c15fb0de2808b493b08dee5b9e0491f09
eagleychen <eagleychen@tencent.com>
2024-03-11 17:27:22 +0800

    update win64

--- .github/script/build_enet_windows.cmd
+++ .github/script/build_enet_windows.cmd
@@ -49,8 +49,8 @@ set sed="C:\Program Files\Git\usr\bin\sed.exe"
 %sed% -i "s/.*VIDEOSDKVERSION.*/static const char * VIDEOSDKVERSION = \"%VIDEO_SDK_VERSION%\";/g" ../iot/link/app_common/app_p2p/appWrapper.h
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
-cmake -DCMAKE_BUILD_TYPE=Release -DENET_NO_STATIC_BINARY=ON -DWITH_DHT=OFF -DBUILD_WITH_FS=ON -DWITH_ZIP=OFF -DENET_VERSION=lts_1.3  -G"Visual Studio 16 2019" -Tv142 -AWin32 ..
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+cmake -DCMAKE_BUILD_TYPE=Release -DENET_NO_STATIC_BINARY=ON -DWITH_DHT=OFF -DBUILD_WITH_FS=ON -DWITH_ZIP=OFF -DENET_VERSION=lts_1.3  -G"Visual Studio 16 2019" -Tv142 -Ax64 ..
 echo ================end-1======================
 cmake --build . --config Release
 echo ================end-2======================
@@ -66,7 +66,7 @@ cp -r ../app_common/utils        app_interface/utils
 mkdir build
 cd build
 
-cmake .. -DCOMPILE_SYSTEM=windows -DSYSTEM_ARCH=x86 -DCOMPILE_TYPE=Release -DCMAKE_BUILD_TYPE=Release  -G"Visual Studio 16 2019" -Tv142 -AWin32 ..
+cmake .. -DCOMPILE_SYSTEM=windows -DSYSTEM_ARCH=x64 -DCOMPILE_TYPE=Release -DCMAKE_BUILD_TYPE=Release  -G"Visual Studio 16 2019" -Tv142 -Ax64 ..
 cmake --build . --config Release
 
 ls -l ../p2p_sample/
