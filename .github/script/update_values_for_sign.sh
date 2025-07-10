df5b26f3949e450f968f01a1d569830fafd4813d
neilyhe <228429803@qq.com>
2025-07-10 16:05:24 +0800

    更新secret

--- .github/script/update_values_for_sign.sh
+++ .github/script/update_values_for_sign.sh
@@ -5,4 +5,4 @@ password=$PASSWORD_OF_SIGN
 root_path=$(pwd)
 sed -i 's#MY_KEY_ID#'$key_id'#g' $1
 sed -i 's#MY_PASSWORD#'$password'#g' $1
-sed -i 's#MY_KEY_RING_FILE#'$root_path'/secring.gpg#g' $1
+sed -i 's#MY_KEY_RING_FILE#'$root_path'/secret.gpg#g' $1
