cd380736275a9d21ccba18119a7a91adfd0e0c80
neilyhe <228429803@qq.com>
2026-06-25 14:22:08 +0800

    修改流水线发布sdk版本号命名

--- .github/script/update_version_for_android.sh
+++ .github/script/update_version_for_android.sh
@@ -15,28 +15,35 @@ echo 333---$rb
 vtag=${currtag#*v}
 echo $vtag
 
-
 branch=${currbra#*v}
-vbranch=${branch%x*}0
-echo $vbranch
 
-function version_ge(){
-    test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1";
-}
+# 判断分支名是否是 vX.Y.x 或 vX.Y.x-fixbug（精确匹配），如果是则走 tag 自增逻辑
+# 其他分支（如 vX.Y.x-log、vX.Y.x-fixbug01 等）使用分支名作为版本号
+if [[ "$branch" =~ ^[0-9]+\.[0-9]+\.x$ ]] || [[ "$branch" =~ ^[0-9]+\.[0-9]+\.x-fixbug$ ]]; then
+    vbranch=${branch%x*}0
+    echo "vbranch: $vbranch"
 
-resultvv=$vbranch
-if version_ge $vtag $vbranch; then
+    function version_ge(){
+        test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1";
+    }
 
-    echo "$vtag is greater than or equal to $vbranch"
+    resultvv=$vbranch
+    if version_ge $vtag $vbranch; then
+        echo "$vtag is greater than or equal to $vbranch"
 
-    vtaglist=(${vtag//./ })
+        vtaglist=(${vtag//./ })
 
-    firsttag=${vtaglist[0]}
-    secondtag=${vtaglist[1]}
-    thirdtag=${vtaglist[2]}
-    thirdtag=`expr $thirdtag + 1`
+        firsttag=${vtaglist[0]}
+        secondtag=${vtaglist[1]}
+        thirdtag=${vtaglist[2]}
+        thirdtag=`expr $thirdtag + 1`
 
-    resultvv=$firsttag.$secondtag.$thirdtag
+        resultvv=$firsttag.$secondtag.$thirdtag
+    fi
+else
+    # 其他分支使用分支名作为版本号
+    resultvv=$branch
+    echo "use branch name as version: $resultvv"
 fi
 
 echo "-->>$resultvv"
